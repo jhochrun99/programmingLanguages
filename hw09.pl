03332c55a8b692819d6444f9f544052e07d5e412
Jess <jhochrun99@gmail.com>
2019-11-21 21:29:34 -0500

    Update hw09.pl

--- hw09.pl
+++ hw09.pl
@@ -1,10 +1,12 @@
-% problem 2.6
+% p6
 inboth(A,B,X) :- member(X,A), member(X,B).
 
-% problem 3.7
+% p7
 increment_all([],[]).
 increment_all([H1|T1], [H2|T2]) :- increment_all(T1,T2), H2 is H1+1.
 
-% problem 3.8 < doesnt work
-foldup([],[]).
-foldup([H|[H2|T]], L) :- foldup([L|T],[L]), L is H+H2.
+% p8
+foldup([], _).
+foldup(L1, []) :- foldup(L1, L1).
+foldup([H|[H2|T]], [H|[H2|T]]) :- L is H+H2, foldup(T, [L|T]).
+foldup([H|T], [H2|_]) :- L is H+H2, foldup(T, [L|T]).
